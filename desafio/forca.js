a9b7fd9e3346b8d34973029a7174a40f78ce1481
snowzzrra <guimotta7@gmail.com>
2022-07-21 00:47:58 -0300

    Finalização do jogo, deixando totalmente funcional. Todas as regras foram seguidas, apenas falta otimização e limpeza no código.

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,15 +1,52 @@
 class Forca {
+  constructor(palavra){
+    this.palavra = palavra
+    this.resultado = new Array(palavra.length).fill("_")
+    this.letrasChutadas = []
+    this.vidas = 6
+    this.estado = "aguardando chute"
+  }
 
-  chutar(letra) { }
+  chutar(letra) {
+    // A fazer: regex para verificar tamanho e se é letra.
+    if (letra.length < 1 || letra.length > 1){
+      return console.log("O seu chute deve conter uma única letra!")
+    }
 
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+    if (this.letrasChutadas.includes(letra)){
+      return console.log("Essa letra já foi chutada!")
+    }
+
+    if (this.palavra.includes(letra)){
+      this.resultado.forEach((element, index) => {
+        if (letra == this.palavra.charAt(index)){
+          this.resultado[index] = letra
+        }
+      })
+
+      this.letrasChutadas.push(letra)
+    } else{
+      this.letrasChutadas.push(letra)
+      this.vidas--
+      if (this.vidas == 0){
+        this.estado = "perdeu"
+      }
+      return console.log("Letra errada!")
+    }
+
+    if (this.resultado.join("") == this.palavra){
+      this.estado = "ganhou"
+    }
+  }
+
+  buscarEstado() { return this.estado; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
-      return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
-      }
+    return {
+      letrasChutadas: this.letrasChutadas, // Deve conter todas as letras chutadas
+      vidas: this.vidas, // Quantidade de vidas restantes
+      palavra: this.resultado.join("") // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+    }
   }
 }
 
