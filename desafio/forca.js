5a5e09f723c998a52c4e78b741022a58c708ff5f
snowzzrra <guimotta7@gmail.com>
2022-07-22 01:00:55 -0300

    regex acrescentado para verificação mais fácil, otimização e polimento do código, verificações de erro e testes finais, tudo ok!

--- desafio/forca.js
+++ desafio/forca.js
@@ -8,35 +8,24 @@ class Forca {
   }
 
   chutar(letra) {
-    // A fazer: regex para verificar tamanho e se é letra.
-    if (letra.length < 1 || letra.length > 1){
-      return console.log("O seu chute deve conter uma única letra!")
-    }
+    letra = letra.toLowerCase()
 
-    if (this.letrasChutadas.includes(letra)){
-      return console.log("Essa letra já foi chutada!")
-    }
+    if (!/^[a-z]{1}$/.test(letra)) { return console.log("O seu chute não é válido.") }
+
+    if (this.letrasChutadas.includes(letra)) { return console.log("Essa letra já foi chutada!") }
+
+    this.letrasChutadas.push(letra)
 
     if (this.palavra.includes(letra)){
-      this.resultado.forEach((element, index) => {
-        if (letra == this.palavra.charAt(index)){
-          this.resultado[index] = letra
-        }
-      })
-
-      this.letrasChutadas.push(letra)
-    } else{
-      this.letrasChutadas.push(letra)
+      this.resultado.forEach((e, index) => letra == this.palavra.charAt(index) ? this.resultado[index] = letra : "")
+    } else {
       this.vidas--
-      if (this.vidas == 0){
-        this.estado = "perdeu"
-      }
+      this.vidas == 0 ? this.estado = "perdeu" : "" 
+
       return console.log("Letra errada!")
     }
 
-    if (this.resultado.join("") == this.palavra){
-      this.estado = "ganhou"
-    }
+    this.resultado.join("") == this.palavra ? this.estado = "ganhou" : ""
   }
 
   buscarEstado() { return this.estado; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
@@ -45,7 +34,7 @@ class Forca {
     return {
       letrasChutadas: this.letrasChutadas, // Deve conter todas as letras chutadas
       vidas: this.vidas, // Quantidade de vidas restantes
-      palavra: this.resultado.join("") // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+      palavra: this.resultado // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
     }
   }
 }
